8389ee5eca5127202c8c250b4eafadbb0cdfaa73
AmirShamim <amirshamim412@gmail.com>
2025-08-26 08:28:38 +0530

    Secondary commit

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(express.urlencoded(false))
 
 app.use('/api/products', productRoute)
 
-PORT = 3000;
+const PORT = process.env.PORT || 5000;
+
 
 app.get("/", (req, res) => {
   res.send("This is some");
